16989d4a4e1d3f1b07e98694d0c466974e3d3824
Christophe Tav <christophe.tav@gmail.com>
2024-04-22 12:21:55 -0400

    update

---
+++ scripts/chipseq_PCa_corepressor_pipeline/01_download_fastq/download_fastq_chipseq_PCa_corepressor.sh
@@ -0,0 +1,31 @@
+mkdir -p /home/chris11/scratch/20240422_PCa_ChIP_corepressor/chipseq_PCa_corepressor/raw_fastq
+cd /home/chris11/scratch/20240422_PCa_ChIP_corepressor/chipseq_PCa_corepressor/raw_fastq
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/065/SRR19639165/SRR19639165_1.fastq.gz -o dl_fastq_SRR19639165_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/065/SRR19639165/SRR19639165_2.fastq.gz -o dl_fastq_SRR19639165_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/064/SRR19639164/SRR19639164_1.fastq.gz -o dl_fastq_SRR19639164_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/064/SRR19639164/SRR19639164_2.fastq.gz -o dl_fastq_SRR19639164_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/063/SRR19639163/SRR19639163_1.fastq.gz -o dl_fastq_SRR19639163_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/063/SRR19639163/SRR19639163_2.fastq.gz -o dl_fastq_SRR19639163_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/062/SRR19639162/SRR19639162_1.fastq.gz -o dl_fastq_SRR19639162_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/062/SRR19639162/SRR19639162_2.fastq.gz -o dl_fastq_SRR19639162_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/061/SRR19639161/SRR19639161_1.fastq.gz -o dl_fastq_SRR19639161_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/061/SRR19639161/SRR19639161_2.fastq.gz -o dl_fastq_SRR19639161_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/060/SRR19639160/SRR19639160_1.fastq.gz -o dl_fastq_SRR19639160_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/060/SRR19639160/SRR19639160_2.fastq.gz -o dl_fastq_SRR19639160_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/052/SRR19639152/SRR19639152_1.fastq.gz -o dl_fastq_SRR19639152_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/052/SRR19639152/SRR19639152_2.fastq.gz -o dl_fastq_SRR19639152_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/051/SRR19639151/SRR19639151_1.fastq.gz -o dl_fastq_SRR19639151_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/051/SRR19639151/SRR19639151_2.fastq.gz -o dl_fastq_SRR19639151_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/050/SRR19639150/SRR19639150_1.fastq.gz -o dl_fastq_SRR19639150_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/050/SRR19639150/SRR19639150_2.fastq.gz -o dl_fastq_SRR19639150_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/049/SRR19639149/SRR19639149_1.fastq.gz -o dl_fastq_SRR19639149_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/049/SRR19639149/SRR19639149_2.fastq.gz -o dl_fastq_SRR19639149_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/048/SRR19639148/SRR19639148_1.fastq.gz -o dl_fastq_SRR19639148_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/048/SRR19639148/SRR19639148_2.fastq.gz -o dl_fastq_SRR19639148_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/047/SRR19639147/SRR19639147_1.fastq.gz -o dl_fastq_SRR19639147_1.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/047/SRR19639147/SRR19639147_2.fastq.gz -o dl_fastq_SRR19639147_2.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/046/SRR19639146/SRR19639146.fastq.gz -o dl_fastq_SRR19639146.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/045/SRR19639145/SRR19639145.fastq.gz -o dl_fastq_SRR19639145.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/044/SRR19639144/SRR19639144.fastq.gz -o dl_fastq_SRR19639144.log &
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR196/043/SRR19639143/SRR19639143.fastq.gz -o dl_fastq_SRR19639143.log &
---
+++ scripts/chipseq_PCa_corepressor_pipeline/02_fastqc_beforetrim/batch_sh/fastqc_beforetrim_LNCaP-XIP_DMSO_AR_R2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=def-stbil30
+#SBATCH --mail-user=christophe.tav@gmail.com
+#SBATCH --mail-type=ALL
+
+
+mkdir -p /home/chris11/projects/def-stbil30/chris11/20240422_PCa_ChIP_corepressor/output/chip-pipeline_PCA_corepressor-GRCh38/fastqc_beforetrim_output/LNCaP-XIP_DMSO_AR_R2
+
+
+/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/fastqc/0.11.9/fastqc --outdir /home/chris11/projects/def-stbil30/chris11/20240422_PCa_ChIP_corepressor/output/chip-pipeline_PCA_corepressor-GRCh38/fastqc_beforetrim_output/LNCaP-XIP_DMSO_AR_R2 --format fastq /home/chris11/projects/def-stbil30/chris11/20240422_PCa_ChIP_corepressor/raw/chipseq_PCa_corepressor/raw_fastq/SRR19639163_2.fastq.gz
